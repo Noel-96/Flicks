f44a0dbe4e291bda15b1701f441234c06cff6181
Noel-96 <noelorion69@gmail.com>
2022-04-21 17:54:23 +0100

    Initial Commit

---
+++ Flicks/FlicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlicksApp.swift
+//  Flicks
+//
+//  Created by Noel Obaseki on 21/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
